3238dcf7ca06a3560526cdce5d2dc07f0222bdf8
Chris <christurner5@gmail.com>
2024-02-25 17:11:46 +0000

    Move flex styling for picker

--- gfr-app/src/components/ScrollQuiz.vue
+++ gfr-app/src/components/ScrollQuiz.vue
@@ -5,9 +5,6 @@ import ScrollUtils, { type Scroll, type ScrollType } from "@/services/ScrollUtil
 let no_scrolls: number = 4;
 let indexes: number[] = [];
 
-// Pick a random number from 1-4 to be the correct answer
-let correctIndex: number = Math.floor(Math.random() * no_scrolls) ;
-
 // Pick random indexes from the scrolls array, ensuring no duplicates
 while (indexes.length < no_scrolls) {
   let n = Math.floor(Math.random() * ScrollUtils.getScrolls().length);
@@ -16,15 +13,33 @@ while (indexes.length < no_scrolls) {
   }
 }
 
+// Pick a random number from 1-4 to be the correct answer
+const correctIndex: number = Math.floor(Math.random() * no_scrolls) ;
+const correctScroll: Scroll = ScrollUtils.getScrolls()[indexes[correctIndex]];
+
+
 </script>
 
 <template>
-  <div class="scroll-detail" v-for="i in indexes" :key="i" >
-    <scroll-detail :index="i" />
+  <div class="scoll-picker">
+    <div class="scroll-detail" v-for="i in indexes" :key="i" >
+      <scroll-detail :index="i" />
+    </div>
   </div>
 </template>
 
 <style scoped>
+
+
+.scoll-picker {
+  display: flex;
+  justify-content: center;
+  align-items: stretch;
+  overflow-x: auto;
+  gap: 20px;
+  width: 100%;
+}
+
 
 .scroll-detail {
   flex-basis: 100%;
