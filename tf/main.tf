d94a42dcb8b0a47cc33e62b41b44e5905c94d4c2
Chris <christurner5@gmail.com>
2024-03-02 15:34:38 +0000

    tf setup for dns

--- tf/main.tf
+++ tf/main.tf
@@ -3,6 +3,16 @@ resource "aws_s3_bucket" "gfr_ui_bucket" {
   bucket = "gfr-ui"
 }
 
+resource "aws_s3_bucket_website_configuration" "gfr_ui_bucket_website" {
+  bucket = aws_s3_bucket.gfr_ui_bucket.bucket
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "error.html"
+  }
+}
+
 resource "aws_s3_bucket_ownership_controls" "gfr_ui_bucket_acl_ownership" {
   bucket = aws_s3_bucket.gfr_ui_bucket.id
   rule {
@@ -26,4 +36,23 @@ resource "aws_s3_bucket_acl" "gfr_ui_bucket_acl" {
     aws_s3_bucket_ownership_controls.gfr_ui_bucket_acl_ownership,
     aws_s3_bucket_public_access_block.gfr_ui_bucket_access,
   ]
+}
+
+
+// Route 53 stuff. Note that my domain was purchased with namecheap
+
+resource "aws_route53_zone" "gfr_zone" {
+  name = "gunfire.pro"
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.gfr_zone.zone_id
+  name    = "gunfire.pro"
+  type    = "A"
+
+  alias {
+    name                   = aws_s3_bucket_website_configuration.gfr_ui_bucket_website.website_domain
+    zone_id                = aws_s3_bucket.gfr_ui_bucket.hosted_zone_id
+    evaluate_target_health = false
+  }
 }
