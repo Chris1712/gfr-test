91e9bfd1d9b2fba1bd139a8b4d32269437fcd619
Chris <christurner5@gmail.com>
2024-03-13 17:18:58 +0000

    add full animation

--- gfr-app/src/components/ScrollQuiz.vue
+++ gfr-app/src/components/ScrollQuiz.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import {computed, ref} from 'vue';
 import ScrollDetail from "@/components/ScrollDetail.vue";
 import ScrollUtils, { type Scroll } from "@/services/ScrollUtils";
 
-type mode = "guess" | "fadeout" | "done";
+type mode = "guess" | "fadeoutWrong" | "fadeOutPicker" | "showCorrect" | "done";
 
 // Number of scrolls to display
 const no_scrolls: number = 4;
@@ -14,10 +14,10 @@ const indexes = ref<number[]>([]);
 const correctIndex = ref<number>();
 // The correct scroll
 const correctScroll = ref<Scroll>();
-//  Index of the scroll guessed by the user; -1 when not guessed, then changes to guessed index
-const guessedScrollIndex = ref<number>(-1);
 
 const currentMode = ref<mode>("guess");
+const hidePicker = computed(() => ! (currentMode.value === "guess" || currentMode.value === "fadeoutWrong"));
+const showAnswer = computed(() => currentMode.value === "showCorrect" || currentMode.value === "done");
 
 pickScrolls();
 
@@ -37,7 +37,7 @@ function pickScrolls() {
 }
 
 function selectScroll(index: number) {
-  currentMode.value = "fadeout";
+  currentMode.value = "fadeoutWrong";
   console.log("Selected scroll: " + index);
   if (index === correctIndex.value) {
     console.log("Correct!");
@@ -46,14 +46,19 @@ function selectScroll(index: number) {
   }
 
   setTimeout(() => {
-    // TODO Once the fadeout is done, remove the wrong elements entirely
-    currentMode.value = "done";
+    currentMode.value = "fadeOutPicker";
+  }, 500);
+
+  setTimeout(() => {
+    currentMode.value = "showCorrect";
   }, 1000);
 
+  setTimeout(() => {
+    currentMode.value = "done";
+  }, 1500);
 }
 
 function reset() {
-  guessedScrollIndex.value = -1;
   currentMode.value = "guess";
   pickScrolls();
 }
@@ -64,21 +69,28 @@ function reset() {
   <h2>Scroll description:</h2>
   <p>{{correctScroll?.desc}}</p>
   <br>
-  <h2 :class="{hide: currentMode !==  'guess'}">Select the matching scroll image:</h2><br>
-  <div id="picker" class="scroll-group">
-    <div class="scroll-item"
-         :class="{
-           'correct': i === correctIndex && currentMode !== 'guess',
-           'hide': i !== correctIndex && currentMode !== 'guess'
-         }"
-         v-for="i in indexes"
-         :key="i"
-         @click="selectScroll(i)">
-      <scroll-detail :index="i" :hide-details="true"/>
+  <div id="picker-container" v-if="!showAnswer">
+    <h2 :class="{hide: hidePicker}">Select the matching scroll image:</h2><br>
+    <div id="picker" class="scroll-group">
+      <div class="scroll-item"
+           :class="{
+             'correct': i === correctIndex && currentMode !== 'guess',
+             'hide': (i !== correctIndex && currentMode !== 'guess') || hidePicker
+           }"
+           v-for="i in indexes"
+           :key="i"
+           @click="selectScroll(i)">
+        <scroll-detail :index="i" :hide-details="true"/>
+      </div>
+    </div>
+  </div>
+  <div id="answer-container" class="scroll-single show" v-if="showAnswer">
+    <div>
+      <scroll-detail :index="correctIndex" :hide-details="false"/>
     </div>
   </div>
   <br>
-  <button :class="{'initiallyHidden': currentMode !== 'done'}" @click="reset()">RESET</button>
+  <button :class="{'initiallyHidden': currentMode !== 'done', show: currentMode === 'done'}" @click="reset()">RESET</button>
 </template>
 
 <style scoped>
@@ -95,6 +107,11 @@ function reset() {
 .scroll-item {
   flex-basis: 100%;
   padding: 10px;
+}
+
+.scroll-single {
+  margin: 0 auto;
+  max-width: 300px;
 }
 
 h2, p {
@@ -123,17 +140,26 @@ button {
   border-color: green !important;
 }
 
-.hide {
-  animation: fadeOut 1s forwards;   /* forwards here means the final keyframe stays applied, IE they stay hidden */
-}
-
 .initiallyHidden {
   visibility: hidden;
+}
+
+.hide {
+  animation: fadeOut 0.5s forwards;   /* forwards here means the final keyframe stays applied, IE they stay hidden */
 }
 
 @keyframes fadeOut {
   0% { opacity: 1; }
   100% { opacity: 0; }
+}
+
+.show {
+  animation: fadeIn 0.5s forwards;   /* forwards here means the final keyframe stays applied, IE they stay hidden */
+}
+
+@keyframes fadeIn {
+  0% { opacity: 0; }
+  100% { opacity: 1; }
 }
 
 </style>
